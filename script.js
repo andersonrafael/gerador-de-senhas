7a7dddc9d485896e9fee971649fc5ed97519f49d
unknown <franciscoandersonrafael@gmail.com>
2022-02-28 21:21:55 -0300

    gerador de senhas v1.4

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ sizePassword.innerHTML = sliderElement.value;
 slider.oninput = function () {
     sizePassword.innerHTML = this.value;
 }
+
+function generatePassword() {
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    containerPassword.classList.remove("hide");
+    Password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyPassword() {
+    alert("Senha copiada!")
+    navigator.clipboard.writeText(novaSenha);
+}
+
